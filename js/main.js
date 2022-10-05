47a16b3a76465fed98d4f6617738c84fdc302956
unknown <marwan.nasser565@gamil.com>
2022-10-05 19:08:44 +0200

    last update

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 // Click tog-show
 if (document.querySelector(".tog-show")) {
-    let togglesShow = document.querySelectorAll(".tog-show");
-    togglesShow.forEach((e) => {
-        let togg = true;
-        e.addEventListener("click", (evt) => {
-            let listItem = document.querySelector(e.getAttribute("data-show"));
-            if (togg == true) {
-                listItem.style.display = "flex";
-                togg = false;
-            } else {
-                listItem.style.display = "none";
-                togg = true;
-            }
-        });
+  let togglesShow = document.querySelectorAll(".tog-show");
+  togglesShow.forEach((e) => {
+    let togg = true;
+    e.addEventListener("click", (evt) => {
+      let listItem = document.querySelector(e.getAttribute("data-show"));
+      if (togg == true) {
+        listItem.style.display = "flex";
+        togg = false;
+      } else {
+        listItem.style.display = "none";
+        togg = true;
+      }
     });
+  });
 }
 
 // scroll top effect
@@ -34,32 +34,32 @@ if (document.querySelector(".tog-show")) {
 
 // print
 if (document.getElementById("prt-content")) {
-    console.log(1);
-    var btnPrtContent = document.getElementById("btn-prt-content");
-    btnPrtContent.addEventListener("click", printDiv);
+  console.log(1);
+  var btnPrtContent = document.getElementById("btn-prt-content");
+  btnPrtContent.addEventListener("click", printDiv);
 
-    function printDiv() {
-        var prtContent = document.getElementById("prt-content");
-        newWin = window.open("");
-        newWin.document.head.replaceWith(document.head.cloneNode(true));
-        newWin.document.body.appendChild(prtContent.cloneNode(true));
-        setTimeout(() => {
-            newWin.print();
-            newWin.close();
-        }, 600);
-    }
+  function printDiv() {
+    var prtContent = document.getElementById("prt-content");
+    newWin = window.open("");
+    newWin.document.head.replaceWith(document.head.cloneNode(true));
+    newWin.document.body.appendChild(prtContent.cloneNode(true));
+    setTimeout(() => {
+      newWin.print();
+      newWin.close();
+    }, 600);
+  }
 }
 
 // loader window
 if (document.querySelector(".loader-container")) {
-    document.body.classList.add("overflow-hidden");
-    const loaderContainer = document.querySelector(".loader-container");
-    window.addEventListener("load", () => {
-        setTimeout(() => {
-            loaderContainer.classList.add("hidden-loader");
-            document.body.classList.remove("overflow-hidden");
-        }, 200);
-    });
+  document.body.classList.add("overflow-hidden");
+  const loaderContainer = document.querySelector(".loader-container");
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      loaderContainer.classList.add("hidden-loader");
+      document.body.classList.remove("overflow-hidden");
+    }, 200);
+  });
 }
 
 
@@ -67,16 +67,20 @@ if (document.querySelector(".loader-container")) {
 const moreInfoBtn = document.querySelectorAll(".more-info");
 
 if (moreInfoBtn) {
-    moreInfoBtn.forEach((ele) => {
-        ele.addEventListener("click", (e) => {
-            e.target.classList.toggle("active")
-            const dataContent = e.target.nextElementSibling;
-            if (dataContent.style.maxHeight) {
-                dataContent.style.maxHeight = null;
-            } else {
-                dataContent.style.maxHeight = dataContent.scrollHeight + "px";
-            }
-            e.preventDefault();
-        });
+  moreInfoBtn.forEach((ele) => {
+    ele.addEventListener("click", (e) => {
+      e.target.classList.toggle("active")
+      const dataContent = e.target.nextElementSibling;
+      if (dataContent.style.maxHeight) {
+        dataContent.style.maxHeight = null;
+      } else {
+        dataContent.style.maxHeight = dataContent.scrollHeight + "px";
+      }
+      e.preventDefault();
     });
+  });
 }
+
+// bootstrap tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
